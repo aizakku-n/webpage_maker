2f45efd4d307151af4ec16938d03309c04f9b6e0
kazuaki.inada <kazuaki.inada@gmail.com>
2018-04-16 18:53:52 +0900

    make base of load json

--- public_html/hoge.js
+++
@@ -1,6 +0,0 @@
-var phonecatApp = angular.module('phonecatApp', ["ngResource"]);
-
-phonecatApp.controller('PhoneListCtrl', ["$scope", "$resource", function ($scope, $resource) {
-    var contents = $resource("./data.json");
-    $scope.phones = contents.query();
-}]);
